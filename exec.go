023affb53a2744c0aaf820358521adbd809e9fab
toby lorne <toby@toby.codes>
2021-01-24 13:07:56 +0000

    main: IsExecutable uses which not command
    because GH actions ubuntu doesn't have "command" installed
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- exec.go
+++ exec.go
@@ -5,7 +5,7 @@ import (
 )
 
 func IsExecutable(name string) bool {
-	cmd := exec.Command("command", "-v", name)
+	cmd := exec.Command("which", name)
 	err := cmd.Run()
 	return err == nil // lazy but fine for now
 }
